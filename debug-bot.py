98fff48e2e393624b4164267af63e972a99daba7
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-16 17:38:45 +0900

    debug

--- debug-bot.py
+++ debug-bot.py
@@ -21,7 +21,7 @@ async def on_message(message):
             await message.author.add_roles(role)
             join = client.get_channel(653923742245978129)
             user_count = sum(1 for member in join.members if not member.bot)
-            await join.send(f"{message.author.name}が参加しました。\n{user_count}人目の参加者です。")
+            await join.send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
             dm = await message.author.create_dm()
             await dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: <#655311853844430858> にて自己紹介をしましょう！\nstep2: <#653919145729064970> から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。")
         else: await message.channel.send("ここでは実行できません。")
@@ -31,9 +31,6 @@ async def on_message(message):
         hantai = '<:hantai:660392595159121959>'
         await message.add_reaction(sansei)
         await message.add_reaction(hantai)
-    # ピン留めのシステムメッセージ削除
-    if message.is_system() == True:
-        await message.delete()
 
 # リアクション追加時の処理一覧
 @client.event
@@ -46,7 +43,7 @@ async def on_raw_reaction_add(payload):
         message = await channel.fetch_message(payload.message_id)
         if message.pinned == False:
             await message.pin()
-            await channel.send(f"{user.name}がメッセージをピン留めしました。")
+            await channel.send(f"{user.name}がピン留めしました。")
 
 # リアクション解除時の処理一覧
 @client.event
