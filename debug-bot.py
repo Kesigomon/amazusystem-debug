28b94119a5c4c678918db992d2feb6178968e7d5
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-16 15:57:10 +0900

    debug

--- debug-bot.py
+++ debug-bot.py
@@ -20,7 +20,7 @@ async def on_raw_reaction_remove(payload):
             message = await channel.fetch_message(payload.message_id)
             if message.pinned == 1:
                 reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
-                if reaction and reaction.count == 0:
+                if reaction and reaction.count == 1:
                     await message.unpin()
                     await message.channel.send(f"メッセージのピン留めを解除しました。")
                 else: await channel.send(f"バグ１")
