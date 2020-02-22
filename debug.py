e9ddc6f75b87356afa3d094bf33f8a6684e5f9e0
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-23 01:03:28 +0900

    test

--- debug.py
+++ debug.py
@@ -1,5 +1,5 @@
-import discord
-import os
+# はじまりのじゅもん
+import discord　import os
 client = discord.Client()
 token = os.environ["TOKEN"]
 
@@ -10,6 +10,23 @@ CH_JOIN = 678511642346258432
 CH_QUESTIONNAIRE = 678585920294748160
 EMOJI_SANSEI = "<:sansei:680682149657051136>"
 EMOJI_HANTAI = "<:hantai:680682184084029460>"
+
+# 各処理の関数化
+def register_func():
+    if not message.channel.id == CH_REGISTER:
+            await message.channel.send("ここでは実行できません。")
+            return
+        role = discord.utils.get(message.guild.roles, name="member")
+        await message.author.add_roles(role)
+        user_count = sum(1 for member in client.get_channel.members if not member.bot)
+        await client.get_channel(CH_JOIN).send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
+        dm = await message.author.create_dm()
+        await dm.send((f"{message.author.mention} アカウントが登録されました。\n"
+                        "まず何をすればいいかわからない方へ▽\n"
+                        "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
+                        "step2: <#653919145729064970> から各サーバーに入室してください！\n"
+                        "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
+                        "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。
 
 # 起動時の処理一覧
 @client.event
@@ -23,24 +40,9 @@ async def on_message(message):
     # BOTからのメッセージを無理
     if message.author.bot:
         return
-
     # アカウント登録処理部
     if message.content == "!register":
-        if not message.channel.id == CH_REGISTER:
-            await message.channel.send("ここでは実行できません。")
-            return
-        role = discord.utils.get(message.guild.roles, name="member")
-        await message.author.add_roles(role)
-        user_count = sum(1 for member in client.get_channel.members if not member.bot)
-        await client.get_channel(CH_JOIN).send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
-        dm = await message.author.create_dm()
-        await dm.send((f"{message.author.mention} アカウントが登録されました。\n"
-                        "まず何をすればいいかわからない方へ▽\n"
-                        "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
-                        "step2: <#653919145729064970> から各サーバーに入室してください！\n"
-                        "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
-                        "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。"))
-
+        register_func()
     # サーバーアンケート処理部
     if message.channel.id == CH_QUESTIONNAIRE:
         await message.add_reaction(EMOJI_SANSEI)
@@ -49,16 +51,12 @@ async def on_message(message):
 # リアクション追加時の処理一覧
 @client.event
 async def on_raw_reaction_add(payload):
+    # BOTからのリアクションを無視
     user = client.get_user(payload.user_id)
     if user.bot:
-        channel = client.get_channel(payload.channel_id)
-        await channel.send("BOTのリアクションです。")
         return
     # ピン留め処理部
     if payload.emoji.name == '📌':
-        user = client.get_user(payload.user_id)
-        if user.bot:
-            return
         channel = client.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
         if message.pinned:
@@ -69,17 +67,17 @@ async def on_raw_reaction_add(payload):
 # リアクション解除時の処理一覧
 @client.event
 async def on_raw_reaction_remove(payload):
+    # BOTからのリアクションを無視
+    user = client.get_user(payload.user_id)
+    if user.bot:
+        return
     # ピン解除処理部
     if payload.emoji.name == '📌':
-        user = client.get_user(payload.user_id)
-        if user.bot:
-            return
         channel = client.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
         if not message.pinned:
             return
-        reaction = discord.utils.get(
-            message.reactions, emoji=payload.emoji.name)
+        reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
         if reaction:
             return
         await message.unpin()
