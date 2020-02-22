b2b593426fcd4ed9a4887919e92cb33f7d3e8207
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-23 05:30:02 +0900

    test

--- debug.py
+++ debug.py
@@ -12,7 +12,11 @@ EMOJI_SANSEI = "<:sansei:680682149657051136>"
 EMOJI_HANTAI = "<:hantai:680682184084029460>"
 
 def do(What):
-    loop = asyncio.set_event_loop()
+    try:
+        loop = asyncio.get_event_loop()
+    except:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
     loop.run_until_complete(What())
 
 async def startup():
