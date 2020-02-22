a0c55619844f0d5c3675b12d7bed1c8c1e9a8d0f
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-23 01:07:56 +0900

    debug

--- debug.py
+++ debug.py
@@ -15,19 +15,19 @@ EMOJI_HANTAI = "<:hantai:680682184084029460>"
 # 各処理の関数化
 def register_func():
     if not message.channel.id == CH_REGISTER:
-            await message.channel.send("ここでは実行できません。")
-            return
-        role = discord.utils.get(message.guild.roles, name="member")
-        await message.author.add_roles(role)
-        user_count = sum(1 for member in client.get_channel.members if not member.bot)
-        await client.get_channel(CH_JOIN).send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
-        dm = await message.author.create_dm()
-        await dm.send((f"{message.author.mention} アカウントが登録されました。\n"
-                        "まず何をすればいいかわからない方へ▽\n"
-                        "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
-                        "step2: <#653919145729064970> から各サーバーに入室してください！\n"
-                        "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
-                        "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。
+        await message.channel.send("ここでは実行できません。")
+        return
+    role = discord.utils.get(message.guild.roles, name="member")
+    await message.author.add_roles(role)
+    user_count = sum(1 for member in client.get_channel.members if not member.bot)
+    await client.get_channel(CH_JOIN).send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
+    dm = await message.author.create_dm()
+    await dm.send((f"{message.author.mention} アカウントが登録されました。\n"
+                    "まず何をすればいいかわからない方へ▽\n"
+                    "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
+                    "step2: <#653919145729064970> から各サーバーに入室してください！\n"
+                    "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
+                    "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。
 
 # 起動時の処理一覧
 @client.event
