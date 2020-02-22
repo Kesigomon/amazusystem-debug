dd0ef25cb3ef9a35d4f8c76ecc8df10b5c06aed1
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-23 01:51:32 +0900

    test
    asyncio.run

--- debug.py
+++ debug.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 client = discord.Client()
 token = os.environ["TOKEN"]
-loop = asyncio.get_running_loop()
+
 CH_STARTUP = 678507923311165443
 CH_REGISTER = 678511640693440526
 CH_JOIN = 678511642346258432
@@ -17,7 +17,7 @@ async def startup():
 @client.event
 async def on_ready():
     # do(startup)
-    loop.run_until_complete(startup())
+    asyncio.run(startup())
 
 @client.event
 async def on_message(message):
