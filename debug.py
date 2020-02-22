e109431d7c28351d1ee926b98093038d371bc295
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-23 01:17:01 +0900

    test

--- debug.py
+++ debug.py
@@ -18,16 +18,16 @@ def register_func():
         message.channel.send("ここでは実行できません。")
         return
     role = discord.utils.get(message.guild.roles, name="member")
-    await message.author.add_roles(role)
+    message.author.add_roles(role)
     user_count = sum(1 for member in client.get_channel.members if not member.bot)
-    await client.get_channel(CH_JOIN).send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
-    dm = await message.author.create_dm()
-    await dm.send((f"{message.author.mention} アカウントが登録されました。\n"
-                    "まず何をすればいいかわからない方へ▽\n"
-                    "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
-                    "step2: <#653919145729064970> から各サーバーに入室してください！\n"
-                    "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
-                    "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。"))
+    client.get_channel(CH_JOIN).send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
+    dm = message.author.create_dm()
+    dm.send((f"{message.author.mention} アカウントが登録されました。\n"
+              "まず何をすればいいかわからない方へ▽\n"
+              "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
+              "step2: <#653919145729064970> から各サーバーに入室してください！\n"
+              "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
+              "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。"))
 
 # 起動時の処理一覧
 @client.event
