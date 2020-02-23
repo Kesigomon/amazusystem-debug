171aca9c5793b0c0092a94ed79b859ff2e89b320
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-23 22:00:33 +0900

    Formatted code with autopep8

--- debug.py
+++ debug.py
@@ -18,15 +18,17 @@ async def register(message):
         return
     role = discord.utils.get(message.guild.roles, name="member")
     await message.author.add_roles(role)
-    user_count = sum(1 for member in client.get_channel(CH_JOIN).members if not member.bot)
+    user_count = sum(1 for member in client.get_channel(
+        CH_JOIN).members if not member.bot)
     await client.get_channel(CH_JOIN).send(f"{message.author.name}が参加しました。\n現在の参加者数は{user_count}人です。")
     dm = await message.author.create_dm()
     await dm.send((f"{message.author.mention} アカウントが登録されました。\n"
-                    "まず何をすればいいかわからない方へ▽\n"
-                    "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
-                    "step2: <#653919145729064970> から各サーバーに入室してください！\n"
-                    "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
-                    "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。"))
+                   "まず何をすればいいかわからない方へ▽\n"
+                   "step1: <#655311853844430858> にて自己紹介をしましょう！\n"
+                   "step2: <#653919145729064970> から各サーバーに入室してください！\n"
+                   "【Tips】スパム防止のため #welcome と #register は非表示になりました。\n"
+                   "そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。"))
+
 
 async def pin(payload, user):
     channel = client.get_channel(payload.channel_id)
@@ -35,6 +37,7 @@ async def pin(payload, user):
         return
     await message.pin()
     await channel.send(f"{user.name}がピン留めしました。")
+
 
 async def unpin(payload, user):
     channel = client.get_channel(payload.channel_id)
@@ -46,13 +49,15 @@ async def unpin(payload, user):
         return
     await message.unpin()
     await channel.send("リアクションがゼロになったため、ピン留めが解除されました。")
-    embed = discord.Embed(title=f"送信者:{message.author}", description=f"メッセージ内容:{message.content}", color=0xff0000)
+    embed = discord.Embed(
+        title=f"送信者:{message.author}", description=f"メッセージ内容:{message.content}", color=0xff0000)
     await channel.send(embed=embed)
 
 # events
 @client.event
 async def on_ready():
     await client.get_channel(CH_STARTUP).send("start up succeed. ")
+
 
 @client.event
 async def on_message(message):
@@ -64,6 +69,7 @@ async def on_message(message):
         await message.add_reaction(EMOJI_SANSEI)
         await message.add_reaction(EMOJI_HANTAI)
 
+
 @client.event
 async def on_raw_reaction_add(payload):
     user = client.get_user(payload.user_id)
@@ -71,6 +77,7 @@ async def on_raw_reaction_add(payload):
         return
     if payload.emoji.name == "\N{PUSHPIN}":
         await pin(payload, user)
+
 
 @client.event
 async def on_raw_reaction_remove(payload):
